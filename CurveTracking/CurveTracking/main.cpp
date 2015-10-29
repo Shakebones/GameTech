3c5d08f5f2e4a74aaf942f48bee285fc46c4cda9
Shakebones <residues.pheromones@gmail.com>
2015-10-29 02:54:12 -0400

    Simple animaton test

--- CurveTracking/CurveTracking/main.cpp
+++ CurveTracking/CurveTracking/main.cpp
@@ -29,30 +29,49 @@ typedef struct _missile
 
 static Mouse g_mousePos;
 static Mouse g_mousePosChange;
+static Missile g_missile;
+CANICurveTracking* g_pAnimation;
+float timeElapsed;
 
 static void Init()
 {
-	bool result;
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0, 1280.0, 0.0, 720.0);
+
+	// Initialize missile parameters
+	g_missile.pos = VFXEpoch::Vector2Df(640, 360);
+	g_missile.vel = VFXEpoch::Vector2Df(1.0f, 1.0f);
+	g_missile.fuel = 10.0f;
+
+	g_pAnimation = new CANICurveTracking;
+	if (!g_pAnimation)
+		exit(-1);
 }
 
 void Draw()
 {
+	clock_t begin = GetTickCount();
 	glPointSize(10.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor4f(.0f, 1.0f, .0f, 1.0f);
+	g_missile.fuel -= timeElapsed;
+	float fuleScalingdown = g_missile.fuel / 100.0f;
+	g_missile.fuel <= 0.0f ? glColor3f(1.0f, 0.2f, 0.1f) : glColor3f(0.0f, fuleScalingdown, 0.0f);
+	
 	glBegin(GL_POINTS);
 
 	// TODO: Animate the points
+	g_missile.pos.m_x += timeElapsed * 24.0f;
+	glVertex3f(g_missile.pos.m_x, 360, 0);
 
 	glEnd();
 	glFlush();
 
 	glutPostRedisplay();
 	glutSwapBuffers();
+	clock_t end = GetTickCount();
+	timeElapsed = (end - begin) / 1000.0f;
 }
 
 void KeyboardEvent(int key, int x, int y)
@@ -81,12 +100,16 @@ static void MouseEvent(int button, int state, int x, int y)
 
 static void MouseMotionFunc(GLint x, GLint y)
 {
-	if (0 == g_mousePos.state) return;
-	g_mousePosChange.x = x;
-	g_mousePosChange.y = y;
-	g_mousePos.x = g_mousePosChange.x;
-	g_mousePos.y = g_mousePosChange.y;
-	cout << "Mouse at <" << g_mousePos.x << "," << g_mousePos.y << ">" << endl;
+	if (0 == g_mousePos.state) 
+		return;
+	else
+	{
+		g_mousePosChange.x = x;
+		g_mousePosChange.y = y;
+		g_mousePos.x = g_mousePosChange.x;
+		g_mousePos.y = g_mousePosChange.y;
+		cout << "Mouse at <" << g_mousePos.x << "," << g_mousePos.y << ">" << endl;
+	}
 }
 
 void Resize(int width, int height)
@@ -96,6 +119,11 @@ void Resize(int width, int height)
 
 void Shutdown()
 {
+	if (g_pAnimation)
+	{
+		delete g_pAnimation;
+		g_pAnimation = NULL;
+	}
 }
 
 int main(int argc, char** argv)
