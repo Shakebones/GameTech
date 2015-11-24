7fba9984b1f2f991f6819bc8c1c939eb4bf80367
Shakebones <residues.pheromones@gmail.com>
2015-11-24 00:52:11 -0330

    It can fire now!

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,19 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Boundary bndry;
     public float tilt;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    void Update()
+    {
+	if (Input.GetButton("Fire1") && Time.time > nextFire)
+	{
+	    nextFire = Time.time + fireRate;
+	    Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+	}
+    }
 
     void FixedUpdate()
     {
